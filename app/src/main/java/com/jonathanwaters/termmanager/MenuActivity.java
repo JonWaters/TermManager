3dc0fdf5cf59f588c7060b4d8b9e8f3270239a2d
Jon Waters <waters1228@gmail.com>
2021-02-25 19:54:17 -0500

    Add TermListActivity

--- app/src/main/java/com/jonathanwaters/termmanager/MenuActivity.java
+++ app/src/main/java/com/jonathanwaters/termmanager/MenuActivity.java
@@ -1,6 +1,10 @@
 package com.jonathanwaters.termmanager;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +14,14 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_menu);
+
+        final Button termsButton = (Button) findViewById(R.id.termsButton);
+        termsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, TermListActivity.class);
+                MenuActivity.this.startActivity(intent);
+            }
+        });
     }
 }

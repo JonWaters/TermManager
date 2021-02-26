639b9c7c82a4f51b921f8603d8ebc9b8b7c88135
Jon Waters <waters1228@gmail.com>
2021-02-25 19:19:40 -0500

    Add MenuActivity

--- app/src/main/java/com/jonathanwaters/termmanager/MainActivity.java
+++ app/src/main/java/com/jonathanwaters/termmanager/MainActivity.java
@@ -1,14 +1,28 @@
 package com.jonathanwaters.termmanager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button enterButton = (Button) findViewById(R.id.enterButton);
+        enterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MenuActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/jonathanwaters/termmanager/MenuActivity.java
@@ -0,0 +1,14 @@
+package com.jonathanwaters.termmanager;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_menu);
+    }
+}

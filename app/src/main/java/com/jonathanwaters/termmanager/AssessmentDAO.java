cc2604259f194d05c147080333f0160f6630c84f
Jon Waters <waters1228@gmail.com>
2021-02-26 15:49:59 -0500

    Add interface AssessmentDAO

---
+++ app/src/main/java/com/jonathanwaters/termmanager/AssessmentDAO.java
@@ -0,0 +1,28 @@
+package com.jonathanwaters.termmanager;
+
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+public interface AssessmentDAO {
+    @Query("SELECT * FROM assessment")
+    List<Assessment> getAll();
+
+    @Query("SELECT * FROM assessment WHERE id = :id")
+    Assessment getByID(int id);
+
+    @Insert
+    void insert(Assessment assessment);
+
+    @Update
+    void update(Assessment assessment);
+
+    @Query("UPDATE assessment SET course_id = :courseID WHERE id = :assessmentID")
+    void updateTermID(int assessmentID, int courseID);
+
+    @Delete
+    void delete(Assessment assessment);
+}

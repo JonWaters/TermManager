c36b6876024f5de78f9c9a20be7d69793ac5807f
Jon Waters <waters1228@gmail.com>
2021-02-27 20:08:37 -0500

    Start work on TermListActivity

--- app/src/main/java/com/jonathanwaters/termmanager/TermListActivity.java
+++ app/src/main/java/com/jonathanwaters/termmanager/TermListActivity.java
@@ -1,14 +1,63 @@
 package com.jonathanwaters.termmanager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TermListActivity extends AppCompatActivity {
+
+    ListView termList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.item_list);
+
+        termList = (ListView) findViewById(R.id.itemListView);
+
+        termList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Intent intent = new Intent(TermListActivity.this, TermDetailActivity.class);
+                TermListActivity.this.startActivity(intent);
+            }
+        });
+
+        populateList();
+    }
+
+    private void populateList() {
+        Database db = Database.getInstance(this);
+        List<Term> allTerms = db.termDAO().getAll();
+        List<String> termNames = new ArrayList<>();
+        String termName;
+
+        for (Term term : allTerms) {
+            termName = term.getName();
+            termNames.add(termName);
+        }
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                termNames
+        );
+
+        termList.setAdapter(arrayAdapter);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        populateList();
     }
 }
